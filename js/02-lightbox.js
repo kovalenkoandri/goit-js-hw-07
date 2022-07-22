f25caa352a3a3f0253d9ba3fbe0b7bc2414b6b77
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-07-22 10:26:24 +0300

    basiclightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,14 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-console.log(galleryItems);
+const ulGallery = document.querySelector('ul[class="gallery"]');
+ulGallery.insertAdjacentHTML(
+  'afterbegin',
+  galleryItems.reduce(
+    (ac, cur) =>
+      ac +
+      `<a class="gallery__item" href="${cur.original}">
+  <img class="gallery__image" src="${cur.preview}" alt="${cur.description}" />
+</a>`,
+    ``
+  )
+);
