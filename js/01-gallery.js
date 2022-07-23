025dc9fd07e7d9c11e39e1b43eee612a7252b346
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-07-23 15:56:43 +0300

    obj settings added

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -15,23 +15,29 @@ divGallery.innerHTML = galleryItems
 </div>`
   )
   .join('');
-// function onEscape(event) { HOW TO MAKE THIS WORK
-//   if (event.key === 'Escape') instance.close();
-// };
+let instance = '';
+function onEscape(event) {
+  if (event.key === 'Escape') instance.close();
+}
 function divGalleryDelegation(ev) {
   //Please note that the image is wrapped in a link,
   //which means that, when clicked, the user will be redirected to another page by default.
   ev.preventDefault(); //Disable this behavior by default.
   if (ev.target.nodeName !== 'IMG') return; //filter delegation or filter click
-  const instance = basicLightbox.create(`
+  instance = basicLightbox.create(
+    `
     <img src="${ev.target.dataset.source}">
-`);
+`,
+    {
+      onShow: instance => {
+        document.addEventListener('keydown', onEscape);
+      },
+      onClose: instance => {
+        document.removeEventListener('keydown', onEscape);
+      },
+    }
+  );
   instance.show();
-  // document.addEventListener('keydown', onEscape);
-  document.addEventListener('keydown', event => {
-    if (event.key === 'Escape') instance.close();
-  });
-};
-
+}
 //2. Implementing delegation to `div.gallery` and getting the `url` of a large image.
 divGallery.addEventListener('click', divGalleryDelegation);
